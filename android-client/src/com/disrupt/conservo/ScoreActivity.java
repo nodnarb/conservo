29c6e79dbd5515c9c62c25f9937d2098acbf5064
Brandon Dorris <brandon@orbotix.com>
2013-04-27 16:10:33 -0600

    Initial Android project
    Added a basic Android project that has no real functionality.

---
+++ android-client/src/com/disrupt/conservo/ScoreActivity.java
@@ -0,0 +1,13 @@
+package com.disrupt.conservo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ *  @author Brandon Dorris
+ */
+public class ScoreActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
